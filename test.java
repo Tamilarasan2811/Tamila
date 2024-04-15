30d4192d6038bc332db4454b312e6ef818e54900
Tamilarasan2811 <167065369+Tamilarasan2811@users.noreply.github.com>
2024-04-15 17:52:58 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,20 @@
+package amazon;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+
+	public static void main(String[] args) {
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		WebElement findElement = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+	
+		
+		
+	}
+
+}
